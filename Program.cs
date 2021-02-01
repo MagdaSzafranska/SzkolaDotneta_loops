4bbb567d981178125413f2c234f86abb3084ee57
MagdaSzafranska <madzia.sz90@gmail.com>
2021-02-01 16:32:24 +0100

    Commit 2 - Pętla while

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace SzkolaDotneta_Loops
     {
         static void Main(string[] args)
         {
-            
+            int a = 0;
+
+            // Pętla WHILE - nie wiemy ile razy dokładnie dane instrukcje będą wykonywane
+            while (a < 10)
+            {
+                Console.WriteLine("Liczba a ma wartość: " + a);
+                a++;
+            }
         }
     }
 }
