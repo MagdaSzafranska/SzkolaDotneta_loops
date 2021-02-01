bdbec2847767e9fd84fd973c02911e8d4827088b
MagdaSzafranska <madzia.sz90@gmail.com>
2021-02-01 16:50:10 +0100

    Commit 3 - Pętla for

--- Program.cs
+++ Program.cs
@@ -6,13 +6,50 @@ namespace SzkolaDotneta_Loops
     {
         static void Main(string[] args)
         {
-            int a = 0;
-
             // Pętla WHILE - nie wiemy ile razy dokładnie dane instrukcje będą wykonywane
-            while (a < 10)
             {
-                Console.WriteLine("Liczba a ma wartość: " + a);
-                a++;
+                /*
+                int a = 0;
+                while (a < 10)
+                {
+                    Console.WriteLine("Liczba a ma wartość: " + a);
+                    a++;
+                }
+            */
+            }
+            
+            Console.WriteLine("");
+
+            // Pętla nieskończona - nie ma warunku wyjścia z pętli
+            {
+                /*
+                while (a % 2 == 0)
+                {
+                    Console.WriteLine("Liczba a: " + a);
+                    a += 2;
+                }
+                */
+            }
+
+            // Pętla DO WHILE - zawsze przynajmniej 1 przejście pętli, bo warunek spr. na końcu
+            {
+            /*
+            int b = 0;
+            do
+            {
+                Console.WriteLine($"Liczba b wynosi: {b}");
+                b++;
+            }
+            while (b < 10);
+                */
+            }
+
+            // Pętla FOR - określona ilość razy wykonania
+            {
+                for (int i = 0; i < 10; i ++)
+                {
+                    Console.WriteLine($"Liczba i: {i}");
+                }    
             }
         }
     }
